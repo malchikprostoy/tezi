dcc3b1d4975253122612ff41d29ddcf64bd0ce92
malchikprostoy <malchikprostoy5@gmail.com>
2024-05-06 11:52:10 +0600

    delete i18next

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './i18next'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>
 );
