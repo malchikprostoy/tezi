53c31802d0cf96dbee22814cfbd4d2d377a80ff4
malchikprostoy <malchikprostoy5@gmail.com>
2024-05-10 20:36:57 +0600

    delete Result page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import Lesson from "./pages/Lesson/Lesson";
 import Russian from "./pages/Exam/Russian/Russian";
-import Result from "./pages/Result/Result";
 import { useState } from "react";
 
 
@@ -16,7 +15,6 @@ function App() {
         <Route path="/" element={<Main setUserName={setUserName}/>}/>
         <Route path="/lesson" element={<Lesson userName={userName}/>}/>
         <Route path="/russian" element={<Russian userName={userName}/>} />
-        <Route path="/result" element={<Result userName={userName}/>}/>
       </Routes>
     </Router>
   );
